34233f6cac5600842ed3dbd73a68e7e1861d25a4
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-04 12:11:20 +0900

    Update [id].vue

--- pages/w/[id].vue
+++ pages/w/[id].vue
@@ -1,17 +1,10 @@
 <template>
 	<div>
 		<h1>{{ $route.params.id }}</h1>
-		{{ counter.count }}
 	</div>
 </template>
 <script setup>
-import { useCounterStore } from '@/store/index'
-
-const counter = useCounterStore()
-
-counter.count++
-// 자동 완성! ✨
-counter.$patch({ count: counter.count + 1 })
-// 또는 actions 사용
-counter.increment()
+import { usePageStore } from '@/store/page'
+const page = usePageStore()
+page.setTitle($route.params.id)
 </script>
