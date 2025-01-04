6c306befc6101bf23f89fb291bbce2cbaa4c3bce
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-04 12:12:13 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
 	<div>
-		<h1>layouts</h1>
+		<h1>{{ page.namespace }}: {{ page.title}}</h1>
 		<slot />
 	</div>
 </template>
+<script setup>
+import { usePageStore } from '@/store/page'
+const page = usePageStore()
+</script>
