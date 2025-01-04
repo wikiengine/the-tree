7618f6a6fcdb8390b419c4dd757363f721509c32
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-04 15:13:34 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <NuxtLoadingIndicator />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+	<div id="app">
+	  <NuxtLoadingIndicator />
+	  <NuxtLayout>
+	    <NuxtPage />
+	  </NuxtLayout>
+	</div>
 </template>
