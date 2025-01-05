b2a082552782e1bb542fe7c70c0ebb4021240251
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-05 22:05:06 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,8 @@
 <template>
-	<div>
-		<nav><NuxtLink to="/">home</NuxtLink> <NuxtLink to="/w/test">test</NuxtLink></nav>
-		<h1>{{ page.namespace }}: {{ page.title}}</h1>
-		<component :is="Liberty">
-			<slot />
-		</component>
-	</div>
+	<component :is="Liberty">
+		<slot />
+	</component>
 </template>
 <script setup>
 import Liberty from "@/skins/liberty/layout.vue"
-import { usePageStore } from '@/store/page'
-const page = usePageStore()
 </script>
