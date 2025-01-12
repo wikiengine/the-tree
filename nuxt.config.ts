d3eb59308ca19339b69ca7a9ea0aa87979e50a99
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-12 21:25:21 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
-  runtimeConfig: {
-    // The private keys which are only available within server-side
-    apiSecret: '374894029',
-    // Keys within public, will be also exposed to the client-side
-  }
+   routeRules: {
+      '/api/**': { public: false },
+    },
+  },
 })
