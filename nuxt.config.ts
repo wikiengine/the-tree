5085dace28e358bc6cdd43768f55c0699e13d635
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-12 21:25:51 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  nitro: {
    routeRules: {
       '/api/**': { public: false },
     },
